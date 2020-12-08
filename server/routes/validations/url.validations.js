5ca8e725f6a364027decba477c0bc142db6782e2
Jacob John Jeevan <jeevan.jacobjohn@gmail.com>
2020-12-08 12:46:19 -0600

    Created validation helpers for URL routes

---
+++ server/routes/validations/url.validations.js
@@ -0,0 +1,21 @@
+const { body, validationResult } = require("express-validator");
+const { handleError } = require("../../helpers/errors");
+
+const postShortenValidation = () => {
+  return [
+    body("url", "Please a provide a url to shorten")
+      .notEmpty()
+      .isString()
+      .trim(),
+  ];
+};
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return handleError(res, 400, errors.array({ onlyFirstError: true })[0]);
+  }
+  next();
+};
+
+module.exports = { postShortenValidation, validate };
