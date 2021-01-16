e5206a075c712b47a5ac98da39af2e5fe1ae4a97
Jacob John Jeevan <jeevan.jacobjohn@gmail.com>
2021-01-15 20:18:06 -0600

    Added toast to app

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { StateWrapper } from "../components/main/AppContext";
 import "../styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <StateWrapper>
-      <Component {...pageProps} />
-    </StateWrapper>
+    <Fragment>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <StateWrapper>
+        <Component {...pageProps} />
+      </StateWrapper>
+    </Fragment>
   );
 }
