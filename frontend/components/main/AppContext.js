586c84ab7134f28941fe2b7c83fed89dafc7f86e
Jacob John Jeevan <jeevan.jacobjohn@gmail.com>
2021-01-15 20:19:29 -0600

    localStorage
    - Changed setUser to store user in localStorage
    - useEffect to fetch user from localStorage if available

--- frontend/components/main/AppContext.js
+++ frontend/components/main/AppContext.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 
 export const defaultState = {
   user: null,
@@ -8,8 +8,21 @@ export const defaultState = {
 export const AppContext = React.createContext(defaultState);
 
 export function StateWrapper({ children }) {
-  const [user, setUser] = useState(defaultState.user);
+  const [user, set] = useState(defaultState.user);
   const [links, setLinks] = useState(defaultState.links);
+
+  const setUser = (user) => {
+    localStorage.setItem("user", user);
+    set(user);
+  };
+
+  useEffect(() => {
+    const foundUser = JSON.parse(localStorage.getItem("user"));
+    if (!user && foundUser) {
+      setUser(foundUser);
+    }
+  }, []);
+
   return (
     <AppContext.Provider value={{ user, setUser, links, setLinks }}>
       {children}
