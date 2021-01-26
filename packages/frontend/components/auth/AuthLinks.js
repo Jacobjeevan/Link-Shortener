deb9ec9b9cd6a9b2160a8f49421fb2a865b80678
Jacob John Jeevan <jeevan.jacobjohn@gmail.com>
2021-01-26 09:37:24 -0600

    CSS styles
    - Extra styling for header and AuthLinks
    	- Mostly with padding/margins to adjust positioning
    - Removed nodemon
    	- Not needed for Next.js
    	- Added port to Next.js dev script

--- packages/frontend/components/auth/AuthLinks.js
+++ packages/frontend/components/auth/AuthLinks.js
@@ -3,24 +3,26 @@ import Link from "next/link";
 
 export default function AuthLinks() {
   return (
-    <div className="container flex space-x-20">
-      <div className="inline-flex p-8 group hover:bg-green-200 shadow-md rounded-md">
-        <Link href="/user/register">
-          <a className="flex flex-col space-y-2 group-hover:text-gray-800">
-            <h3 className="flex-1 text-xl">Register</h3>
+    <div className="container flex space-x-20 items-center">
+      <Link href="/user/register">
+        <a className="flex-1 inline-flex p-8 group hover:bg-green-200 shadow-md rounded-md">
+          <div className="flex flex-col space-y-2 group-hover:text-gray-800">
+            <h3 className="flex-1 text-3xl">Register</h3>
             <p className="flex-1">If you are a new user.</p>
-          </a>
-        </Link>
-      </div>
+          </div>
+        </a>
+      </Link>
 
-      <div className="inline-flex p-8 group hover:bg-green-200 shadow-md rounded-md">
-        <Link href="/user/login">
-          <a className="flex flex-col space-y-2 group-hover:text-gray-800">
-            <h3 className="flex-1 text-xl">Login</h3>
+      <div className="flex-1"></div>
+
+      <Link href="/user/login">
+        <a className="flex-1 inline-flex p-8 group hover:bg-green-200 shadow-md rounded-md">
+          <div className="flex flex-col space-y-2 group-hover:text-gray-800">
+            <h3 className="flex-1 text-3xl">Login</h3>
             <p className="flex-1">If you are an existing user.</p>
-          </a>
-        </Link>
-      </div>
+          </div>
+        </a>
+      </Link>
     </div>
   );
 }
--- packages/frontend/components/layout/Header.js
+++ packages/frontend/components/layout/Header.js
@@ -11,8 +11,10 @@ export default function Header() {
   };
   return (
     <div className="flex px-8 py-3 items-stretch bg-black w-full shadow-md border-b-4 border-green-400">
-      <h1 className="flex-1 text-3xl text-white uppercase">Jeevan Link</h1>
-      <div className="flex-1 flex items-center">
+      <h1 className="flex-1 ml-10 text-3xl text-white uppercase">
+        Jeevan Link
+      </h1>
+      <div className="flex-1 mr-10 flex items-center">
         <p className="flex-1 text-sm text-right text-white">
           A Url Shortening Service
         </p>
