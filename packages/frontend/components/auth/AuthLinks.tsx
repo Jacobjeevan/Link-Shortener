31c4a04550a0b878dbb64fcd20c48f77b0bbcad8
Jacob John Jeevan <jeevan.jacobjohn@gmail.com>
2021-02-12 10:56:40 -0600

    AuthLinks Styling
    - Added breakpoints for responsiveness (mobile screens) for AuthLinks

--- packages/frontend/components/auth/AuthLinks.tsx
+++ packages/frontend/components/auth/AuthLinks.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function AuthLinks(): JSX.Element {
   return (
-    <div className="container flex space-x-20 items-center">
+    <div className="mx-6 flex md:flex-row sm:flex-col md:space-x-5 flex-wrap items-center">
       <Link href="/user/register">
-        <a className="flex-1 inline-flex p-8 group hover:bg-green-200 shadow-md rounded-md">
+        <a className="flex-auto inline-flex flex-wrap p-6 group hover:bg-green-200 shadow-md rounded-md">
           <div className="flex flex-col space-y-2 group-hover:text-gray-800">
             <h3 className="flex-1 text-3xl">Register</h3>
             <p className="flex-1">If you are a new user.</p>
@@ -13,10 +13,8 @@ export default function AuthLinks(): JSX.Element {
         </a>
       </Link>
 
-      <div className="flex-1"></div>
-
       <Link href="/user/login">
-        <a className="flex-1 inline-flex p-8 group hover:bg-green-200 shadow-md rounded-md">
+        <a className="flex-auto inline-flex flex-wrap p-6 group hover:bg-green-200 shadow-md rounded-md">
           <div className="flex flex-col space-y-2 group-hover:text-gray-800">
             <h3 className="flex-1 text-3xl">Login</h3>
             <p className="flex-1">If you are an existing user.</p>
