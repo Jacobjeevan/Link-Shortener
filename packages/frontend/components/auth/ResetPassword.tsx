a00966046cecdbc8c1c371f4ffc7e89e84c16b92
Jacob John Jeevan <jeevan.jacobjohn@gmail.com>
2021-03-16 18:44:33 -0500

    ResetPassword Component
    - Component gets token (passed in as prop from parent)
    - useEffect to check tokenValidity with backend
    	- Displays error message if token is invalid
    	- If not, renders form to input new password + confirmation to resetting password

---
+++ packages/frontend/components/auth/ResetPassword.tsx
@@ -0,0 +1,66 @@
+import React, { Fragment, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { errorClass, formClass, inputClass, resetPasswordResolver, submitBtnClass } from "./AuthHelpers";
+import { checkTokenValidity, changePassword } from "./AuthApi";
+import { toast } from "react-toastify";
+
+export default function ResetPassword(Props: { token: string }): JSX.Element {
+  const { register, handleSubmit, errors } = useForm({
+    resolver: resetPasswordResolver,
+  });
+  const [tokenInvalid, setTokenInvalid] = useState(false);
+
+  const { token } = Props;
+
+  useEffect(() => {
+    async function tokenValidity() {
+      if (token) {
+        const response = await checkTokenValidity(token);
+        const { error } = response;
+        if (error) {
+          setTokenInvalid(true);
+        }
+      }
+    }
+    tokenValidity();
+  }, [token]);
+
+  const onSubmit = async (data: { password: string }) => {
+    const { password } = data;
+    const response = await changePassword(token, password);
+    const { success, error } = response;
+    if (success) {
+      toast.success("Password updated. You may now login with new password.");
+    } else if (error) {
+      toast.error(error);
+    }
+  };
+
+  if (tokenInvalid) {
+    return (
+      <div className="flex flex-col space-y-3 items-center text-3xl">
+        Your reset link is invalid or has expired. Please try again.
+      </div>
+    );
+  }
+
+  return (
+    <Fragment>
+      <form onSubmit={handleSubmit(onSubmit)} className={formClass}>
+        <input name="password" ref={register} placeholder="New Password" className={inputClass} type="password" />
+        <p className={errorClass}>{errors.password?.message}</p>
+
+        <input
+          name="passwordConfirm"
+          ref={register}
+          placeholder="Confirm New Password"
+          className={inputClass}
+          type="password"
+        />
+        <p className={errorClass}>{errors.passwordConfirm?.message}</p>
+
+        <input type="submit" className={submitBtnClass} value="Change Password" />
+      </form>
+    </Fragment>
+  );
+}
