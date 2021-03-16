2febc4f4af15ffa301d7143aa69353d1c5a6400a
Jacob John Jeevan <jeevan.jacobjohn@gmail.com>
2021-03-16 18:39:57 -0500

    RequestPasswordReset Component
    - Form to submit email for password reset request
    - uses requestPasswordReset to call backend (sends email for reset request)
    - uses toast to notify user about success/error

---
+++ packages/frontend/components/auth/RequestPasswordReset.tsx
@@ -0,0 +1,33 @@
+import React, { Fragment } from "react";
+import { useForm } from "react-hook-form";
+import { errorClass, formClass, inputClass, requestResetResolver, submitBtnClass } from "./AuthHelpers";
+import { requestPasswordReset } from "./AuthApi";
+import { toast } from "react-toastify";
+
+export default function RequestPasswordReset(): JSX.Element {
+  const { register, handleSubmit, errors } = useForm({
+    resolver: requestResetResolver,
+  });
+
+  const onSubmit = async (data: { email: string }) => {
+    const { email } = data;
+    const response = await requestPasswordReset(email);
+    const { success, error } = response;
+    if (success) {
+      toast.success("A reset link has been sent to your email.");
+    } else if (error) {
+      toast.error(error);
+    }
+  };
+
+  return (
+    <Fragment>
+      <form onSubmit={handleSubmit(onSubmit)} className={formClass}>
+        <input name="email" ref={register} placeholder="Email/Username" className={inputClass} />
+        <p className={errorClass}>{errors.email?.message}</p>
+
+        <input type="submit" className={submitBtnClass} value="Request Reset" />
+      </form>
+    </Fragment>
+  );
+}
