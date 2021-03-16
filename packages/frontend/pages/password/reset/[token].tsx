1a8c70edf76656276e55765e8f48122c0b715ac9
Jacob John Jeevan <jeevan.jacobjohn@gmail.com>
2021-03-16 18:46:28 -0500

    Password Reset Pages
    - index
    	- Renders RequestPasswordReset component
    - token
    	- Renders ResetPassword component
    		- Passes in token as the prop (useRouter hook to fetch token from query)

--- packages/frontend/pages/password/reset/[token].tsx
+++ packages/frontend/pages/password/reset/[token].tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { useRouter } from "next/router";
+import ResetPassword from "../../../components/auth/ResetPassword";
+import Main from "../../../components/layout/Main";
 
 export default function ChangePassword(): JSX.Element {
-  return <div></div>;
+  const router = useRouter();
+  const { token } = router.query;
+  return <Main Body={() => <ResetPassword token={token as string} />} />;
 }
