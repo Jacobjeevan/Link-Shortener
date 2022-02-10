ecdead736f936779364386dddae61fe6efda8cfc
Jacob John Jeevan <mail@jacobjeevan.me>
2022-02-09 23:17:03 -0600

    AuthApi refactor
    - Slight refactor from async/await to thenable
    - TODO: Change api calls to thenable (in login, dashboard)
    - TODO: Refactor links api to thenable

--- packages/frontend/components/auth/AuthApi.tsx
+++ packages/frontend/components/auth/AuthApi.tsx
@@ -1,8 +1,29 @@
 import { axiosInstance } from "../utils/axios";
-import { AxiosResponse } from "axios";
+import { AxiosError, AxiosResponse } from "axios";
 import { IAuth, IAuthResponse, IResetReponse, IRequestResetResponse } from "./types/auth";
 
-export async function login(loginInfo: IAuth): Promise<IAuthResponse> {
+const responseBody = (response: AxiosResponse) => response.data;
+const errorBody = (error: AxiosError) => error.response;
+
+export const login = (loginInfo: IAuth): Promise<IAuthResponse> =>
+  axiosInstance.post("login/", loginInfo).then(responseBody).catch(errorBody);
+
+export const logout = (): Promise<IAuthResponse> => axiosInstance.get("logout/").then(responseBody).catch(errorBody);
+
+export const register = (registerInfo: string): Promise<IAuthResponse> =>
+  axiosInstance.post("/register", registerInfo).then(responseBody).catch(errorBody);
+
+export const getUser = (): Promise<IAuthResponse> => axiosInstance.get("user/").then(responseBody).catch(errorBody);
+
+export const requestPasswordReset = (email: string): Promise<IRequestResetResponse> =>
+  axiosInstance.post("/password/reset/", email).then(responseBody).catch(errorBody);
+
+export const checkTokenValidity = (token: string): Promise<IResetReponse> =>
+  axiosInstance.get(`password/reset/${token}`).then(responseBody).catch(errorBody);
+
+export const changePassword = (token: string, password: string): Promise<IResetReponse> =>
+  axiosInstance.post(`password/reset/${token}`, { password }).then(responseBody).catch(errorBody);
+/* export async function login(loginInfo: IAuth): Promise<IAuthResponse> {
   let response: AxiosResponse<IAuthResponse>;
   try {
     response = await axiosInstance.post<IAuthResponse>("login/", loginInfo);
@@ -10,7 +31,7 @@ export async function login(loginInfo: IAuth): Promise<IAuthResponse> {
     response = error.response;
   }
   return response.data;
-}
+} 
 
 export async function logout(): Promise<IAuthResponse> {
   let response: AxiosResponse<IAuthResponse>;
@@ -20,7 +41,7 @@ export async function logout(): Promise<IAuthResponse> {
     response = error.response;
   }
   return response.data;
-}
+} 
 
 export async function register(registerInfo: IAuth): Promise<IAuthResponse> {
   let response: AxiosResponse<IAuthResponse>;
@@ -31,6 +52,7 @@ export async function register(registerInfo: IAuth): Promise<IAuthResponse> {
   }
   return response.data;
 }
+
 
 export async function getUser(): Promise<IAuthResponse> {
   let response: AxiosResponse<IAuthResponse>;
@@ -52,6 +74,7 @@ export async function requestPasswordReset(email: string): Promise<IRequestReset
   return response.data;
 }
 
+
 export async function checkTokenValidity(token: string): Promise<IResetReponse> {
   let response: AxiosResponse<IResetReponse>;
   try {
@@ -71,3 +94,4 @@ export async function changePassword(token: string, password: string): Promise<I
   }
   return response.data;
 }
+*/
