53a153f3efde8a8ffd2e57f13d65eca8772649da
Jacob John Jeevan <mail@jacobjeevan.me>
2022-02-10 15:31:03 -0600

    modified AppProps import

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -3,7 +3,7 @@ import { StateWrapper } from "../components/main/AppContext";
 import "../styles/globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { AppProps } from "next/dist/next-server/lib/router/router";
+import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
