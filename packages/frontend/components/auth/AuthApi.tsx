9d6fab6f20ad35f1c721b6e964d8e017ad97041d
Jacob John Jeevan <mail@jacobjeevan.me>
2022-02-10 01:15:58 -0600

    Removed old code
    - Removed the commented out/old code
    - Modified errorResponse to return error data when available

--- packages/frontend/components/auth/AuthApi.tsx
+++ packages/frontend/components/auth/AuthApi.tsx
@@ -3,7 +3,7 @@ import { AxiosError, AxiosResponse } from "axios";
 import { IAuth, IAuthResponse, IResetReponse, IRequestResetResponse } from "./types/auth";
 
 const responseBody = (response: AxiosResponse) => response.data;
-const errorBody = (error: AxiosError) => error.response;
+const errorBody = (error: AxiosError) => error.response?.data;
 
 export const login = (loginInfo: IAuth): Promise<IAuthResponse> =>
   axiosInstance.post("login/", loginInfo).then(responseBody).catch(errorBody);
@@ -23,75 +23,3 @@ export const checkTokenValidity = (token: string): Promise<IResetReponse> =>
 
 export const changePassword = (token: string, password: string): Promise<IResetReponse> =>
   axiosInstance.post(`password/reset/${token}`, { password }).then(responseBody).catch(errorBody);
-/* export async function login(loginInfo: IAuth): Promise<IAuthResponse> {
-  let response: AxiosResponse<IAuthResponse>;
-  try {
-    response = await axiosInstance.post<IAuthResponse>("login/", loginInfo);
-  } catch (error) {
-    response = error.response;
-  }
-  return response.data;
-} 
-
-export async function logout(): Promise<IAuthResponse> {
-  let response: AxiosResponse<IAuthResponse>;
-  try {
-    response = await axiosInstance.get<IAuthResponse>("logout/");
-  } catch (error) {
-    response = error.response;
-  }
-  return response.data;
-} 
-
-export async function register(registerInfo: IAuth): Promise<IAuthResponse> {
-  let response: AxiosResponse<IAuthResponse>;
-  try {
-    response = await axiosInstance.post<IAuthResponse>("register/", registerInfo);
-  } catch (error) {
-    response = error.response;
-  }
-  return response.data;
-}
-
-
-export async function getUser(): Promise<IAuthResponse> {
-  let response: AxiosResponse<IAuthResponse>;
-  try {
-    response = await axiosInstance.get<IAuthResponse>("user/");
-  } catch (error) {
-    response = error.response;
-  }
-  return response.data;
-}
-
-export async function requestPasswordReset(email: string): Promise<IRequestResetResponse> {
-  let response: AxiosResponse<IRequestResetResponse>;
-  try {
-    response = await axiosInstance.post<IRequestResetResponse>("password/reset/", { email });
-  } catch (error) {
-    response = error.response;
-  }
-  return response.data;
-}
-
-
-export async function checkTokenValidity(token: string): Promise<IResetReponse> {
-  let response: AxiosResponse<IResetReponse>;
-  try {
-    response = await axiosInstance.get<IResetReponse>(`password/reset/${token}`);
-  } catch (error) {
-    response = error.response;
-  }
-  return response.data;
-}
-
-export async function changePassword(token: string, password: string): Promise<IResetReponse> {
-  let response: AxiosResponse<IResetReponse>;
-  try {
-    response = await axiosInstance.post<IResetReponse>(`password/reset/${token}`, { password });
-  } catch (error) {
-    response = error.response;
-  }
-  return response.data;
-}
-*/
--- packages/frontend/components/links/LinksApi.tsx
+++ packages/frontend/components/links/LinksApi.tsx
@@ -1,13 +1,13 @@
 import { axiosInstance } from "../utils/axios";
 import useSWR from "swr";
-import axios, { AxiosError, AxiosResponse } from "axios";
+import { AxiosError, AxiosResponse } from "axios";
 import { IShortenAPI, IShortenResponse } from "./types/shorten";
 import { IDeleteResponse } from "./types/deleteLink";
 import { IGetLongUrlResponse } from "./types/getLongUrl";
 import { ILink, IGetLinksResponse, IGetLinksAxiosResponse } from "./types/getLinks";
 
 const responseBody = (response: AxiosResponse) => response.data;
-const errorBody = (error: AxiosError) => error.response;
+const errorBody = (error: AxiosError) => error.response?.data;
 
 export const shorten = (body: IShortenAPI): Promise<IShortenResponse> =>
   axiosInstance.post("shorten/", body).then(responseBody).catch(errorBody);
@@ -18,26 +18,6 @@ export const getLongUrl = (shortUrl: string): Promise<IGetLongUrlResponse> =>
 export const deleteShortUrl = (urlId: string): Promise<IDeleteResponse> =>
   axiosInstance.post("/delete", { urlId }).then(responseBody).catch(errorBody);
 
-/* export async function shorten(body: IShortenAPI): Promise<IShortenResponse> {
-  let response: AxiosResponse<IShortenResponse>;
-  try {
-    response = await axiosInstance.post<IShortenResponse>("shorten/", body);
-  } catch (error) {
-    response = error.response;
-  }
-  return response.data;
-} */
-
-/* export async function getLongUrl(shortUrl: string): Promise<IGetLongUrlResponse> {
-  let response: AxiosResponse<IGetLongUrlResponse>;
-  try {
-    response = await axios.get<IGetLongUrlResponse>(`${process.env.BACKEND_URL}/${shortUrl}`);
-  } catch (error) {
-    response = error.response;
-  }
-  return response.data;
-} */
-
 export function useGetLinks(shouldFetch: boolean): IGetLinksResponse {
   const fetcher = () => axiosInstance.get<IGetLinksAxiosResponse>("/all/").then((res) => res.data);
   const { data, error, mutate } = useSWR(shouldFetch ? "links" : null, fetcher);
@@ -47,16 +27,3 @@ export function useGetLinks(shouldFetch: boolean): IGetLinksResponse {
   }
   return { links, fetchError: error, isLoading: !error && !links, mutate };
 }
-
-/* export async function deleteShortUrl(urlId: string): Promise<IDeleteResponse> {
-  let response: AxiosResponse<IDeleteResponse>;
-  try {
-    response = await axiosInstance.post<IDeleteResponse>("/delete", {
-      urlId,
-    });
-  } catch (error) {
-    response = error.response;
-  }
-  return response.data;
-}
- */
